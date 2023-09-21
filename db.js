19f685989efd75a637a18449afb739df17fc9203
guimaraes12 <guimaraes.b@gsuite.iff.edu.br>
2023-09-21 10:38:52 -0300

    mongo

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://guimaraesb:<password>@cluster0.mfpgrks.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi:
-   {
+  serverApi:{
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
