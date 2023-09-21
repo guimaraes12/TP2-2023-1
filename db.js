1467bc0c29e5e0e728c7ee91a7f488e119e56656
guimaraes12 <guimaraes.b@gsuite.iff.edu.br>
2023-09-21 10:38:34 -0300

    mongo

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://guimaraesb:<password>@cluster0.mfpgrks.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
+  serverApi:
+   {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
@@ -18,7 +19,7 @@ async function run() {
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
-    
+
   }
 }
 run().catch(console.dir);
